a9416ee03842b8676e0e1c04fbfb7367b107d955
neoscript <neoscripting@gmail.com>
2025-11-28 04:27:17 -0600

    feat: utilize tokio to process each entry asynchronously

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, fs, path::Path, time::Instant};
+use std::{collections::HashMap, fs, path::Path, process::exit, sync::Arc, time::Instant};
+
+use tokio::{sync::Mutex, task::JoinSet};
 
 use crate::{
     data_models::{
@@ -9,48 +11,60 @@ use crate::{
 pub mod data_models;
 pub mod third_party;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data_file_path: &Path = Path::new("src/stock_data.json");
     let json_str = fs::read_to_string(data_file_path).expect("should be able to read file");
     let company_data: Vec<CompanyInformation> =
         serde_json::from_str(&json_str).expect("should be able to deserialize json str");
-    let mut category_statistics: HashMap<StockCategory, CategoryProcessingResults> = HashMap::new();
-    seed_category_statistics(&mut category_statistics);
+    let category_statistics: Arc<Mutex<HashMap<StockCategory, CategoryProcessingResults>>> =
+        Arc::new(Mutex::new(HashMap::new()));
 
-    println!("{:?}", category_statistics);
+    let mut guarded_stats = category_statistics.lock().await;
+    seed_category_statistics(&mut guarded_stats);
+    println!("{:?}", guarded_stats);
 
     let start_time = Instant::now();
+
+    let mut tasks = JoinSet::new();
     for c in company_data {
-        let result = do_some_processing(&c);
-        let category_to_lookup: Result<StockCategory, &str> = c.category.as_str().try_into();
+        let guarded_stats = Arc::clone(&category_statistics);
+        tasks.spawn(async move {
+            let result = do_some_processing(&c).is_ok();
+            let category_to_lookup: Result<StockCategory, &str> = c.category.as_str().try_into();
 
-        if category_to_lookup.is_err() {
-            println!("Failed to determine category, skipping: {:?}", c);
-            continue;
-        }
+            if category_to_lookup.is_err() {
+                println!("Failed to determine category, skipping: {:?}", c);
+                return;
+            }
 
-        let category_to_lookup = category_to_lookup.expect("should have determined category");
-        match result {
-            Ok(_) => {
-                let reference = category_statistics
-                    .get_mut(&category_to_lookup)
-                    .expect("should find category");
-                reference.processed += 1;
+            let category_to_lookup = category_to_lookup.expect("should have determined category");
+            match result {
+                true => {
+                    let mut locked_stats = guarded_stats.lock().await;
+                    locked_stats
+                        .get_mut(&category_to_lookup)
+                        .expect("should find category")
+                        .processed += 1;
+                }
+                false => {
+                    let mut locked_stats = guarded_stats.lock().await;
+                    locked_stats
+                        .get_mut(&category_to_lookup)
+                        .expect("should find category")
+                        .processed += 1;
+                }
             }
-            Err(_) => {
-                let reference = category_statistics
-                    .get_mut(&category_to_lookup)
-                    .expect("should find category");
-                reference.failed += 1;
-            }
-        }
+        });
     }
 
+    tasks.join_all().await;
     let end_time = Instant::now();
     let total_processing_time = end_time.duration_since(start_time);
     println!("total_processing_time = {:?}", total_processing_time);
 
     println!("results");
+    let category_statistics = category_statistics.lock().await;
     for (key, val) in category_statistics.iter() {
         println!("Category: {:?} -> Results: {:?}", key, val);
     }

55d56fcb5721c7017d07bc97b9a2ef9523570ec2
neoscript <neoscripting@gmail.com>
2025-11-28 05:00:11 -0600

    feat: improved stats at the end of processing all values

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         serde_json::from_str(&json_str).expect("should be able to deserialize json str");
     let category_statistics: Arc<Mutex<HashMap<StockCategory, CategoryProcessingResults>>> =
         Arc::new(Mutex::new(HashMap::new()));
+    let total_passed = Arc::new(Mutex::new(0));
+    let total_failed = Arc::new(Mutex::new(0));
 
-    let mut guarded_stats = category_statistics.lock().await;
-    seed_category_statistics(&mut guarded_stats);
-    println!("{:?}", guarded_stats);
+    {
+        let mut guarded_stats = category_statistics.lock().await;
+        seed_category_statistics(&mut guarded_stats);
+        println!("{:?}", guarded_stats);
+    }
 
     let start_time = Instant::now();
 
     let mut tasks = JoinSet::new();
     for c in company_data {
         let guarded_stats = Arc::clone(&category_statistics);
+        let guarded_passed = Arc::clone(&total_passed);
+        let guarded_failed = Arc::clone(&total_failed);
         tasks.spawn(async move {
             let result = do_some_processing(&c).is_ok();
-            let category_to_lookup: Result<StockCategory, &str> = c.category.as_str().try_into();
 
+            println!("result: {}", result);
+            let category_to_lookup: Result<StockCategory, &str> = c.category.as_str().try_into();
             if category_to_lookup.is_err() {
                 println!("Failed to determine category, skipping: {:?}", c);
-                return;
+                let mut locked_failed = guarded_failed.lock().await;
+                *locked_failed += 1;
+                return Err("failed category check");
             }
 
             let category_to_lookup = category_to_lookup.expect("should have determined category");
@@ -46,15 +55,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .get_mut(&category_to_lookup)
                         .expect("should find category")
                         .processed += 1;
+
+                    let mut locked_passed = guarded_passed.lock().await;
+                    *locked_passed += 1;
                 }
                 false => {
                     let mut locked_stats = guarded_stats.lock().await;
                     locked_stats
                         .get_mut(&category_to_lookup)
                         .expect("should find category")
-                        .processed += 1;
+                        .failed += 1;
+
+                    let mut locked_failed = guarded_failed.lock().await;
+                    *locked_failed += 1;
                 }
             }
+            Ok(())
         });
     }
 
@@ -63,10 +79,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let total_processing_time = end_time.duration_since(start_time);
     println!("total_processing_time = {:?}", total_processing_time);
 
-    println!("results");
-    let category_statistics = category_statistics.lock().await;
-    for (key, val) in category_statistics.iter() {
-        println!("Category: {:?} -> Results: {:?}", key, val);
+    {
+        println!("results");
+        let category_statistics = category_statistics.lock().await;
+        let keys = [
+            &StockCategory::CategoryA,
+            &StockCategory::CategoryB,
+            &StockCategory::CategoryC,
+            &StockCategory::CategoryD,
+            &StockCategory::CategoryE,
+        ];
+        for key in keys {
+            let val = category_statistics.get(key).expect("should find category");
+            println!("Category: {:?} -> Results: {:?}", key, val);
+        }
     }
+
+    {
+        let passed = total_passed.lock().await;
+        println!("total_passed = {}", *passed);
+    }
+
+    {
+        let failed = total_failed.lock().await;
+        println!("total_failed= {}", *failed);
+    }
+
     Ok(())
 }
--- src/third_party.rs
+++ src/third_party.rs
@@ -4,11 +4,11 @@ use crate::data_models::{CompanyInformation, StockCategory};
 
 pub fn do_some_processing(company_info: &CompanyInformation) -> Result<u64, Box<dyn Error>> {
     let min_sleep_time = 100;
-    let max_sleep_time = 500;
+    let max_sleep_time = 700;
 
-    let timeout_after = ((max_sleep_time as f64) * 0.90) as u64;
+    let timeout_after = 600;
 
-    let random_sleep_timer = rand::random_range(min_sleep_time..max_sleep_time);
+    let random_sleep_timer = rand::random_range(min_sleep_time..=max_sleep_time);
     if random_sleep_timer > timeout_after {
         return Err("Timed out".into());
     }
@@ -18,8 +18,11 @@ pub fn do_some_processing(company_info: &CompanyInformation) -> Result<u64, Box<
         company_info.symbol, company_info.category
     );
     thread::sleep(Duration::from_millis(random_sleep_timer));
-
     let _mapped_category: StockCategory = company_info.category.as_str().try_into()?;
 
+    println!(
+        "got a result for: {}, {} in {} ms",
+        company_info.symbol, company_info.category, random_sleep_timer
+    );
     Ok(random_sleep_timer)
 }
